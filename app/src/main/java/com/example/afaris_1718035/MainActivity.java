574c83373fd14b9e0753cbabf41709f90c4ff1e0
mochammadfaris <1718035@scholar.itn.ac.id>
2019-12-26 22:33:07 +0700

    ini punya Mochammad Faris

---
+++ app/src/main/java/com/example/afaris_1718035/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.afaris_1718035;
+
+        import androidx.annotation.NonNull;
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.provider.Settings;
+        import android.view.Menu;
+        import android.view.MenuItem;
+
+        import com.example.afaris_1718035.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuprofile, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.setting){
+            Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
